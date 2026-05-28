140bdf8bba3ce6e581ee262f38bcdc6a14f6c22e
Akshaj Sinha <akshaj17@gmail.com>
2026-05-28 17:11:42 -0400

    data cleaning

--- wc2022.py
+++ wc2022.py
@@ -6,7 +6,21 @@ df= pd.read_csv("worldcup2022.csv")
 print(df.isnull().sum())
 
 #context of data
+"""
 print(df.shape)
 print(df.info())
 print(df.describe())
+print(df['possession team1'].head())
+"""
+
+#cleaning
+print(df.info()) #index 50 and 54 have spacing issue, 25-27 have spacing issue
+df.columns.str.strip("'").str.strip()
+df.columns = df.columns.str.replace(r'\s+', ' ', regex=True)
+df.rename(columns={'completed line breaksteam1': 'completed line breaks team1',
+                   'completed defensive line breaksteam1': 'completed defensive line breaks team1'},
+          inplace=True)
+print(df.columns.tolist())
+
+
 
