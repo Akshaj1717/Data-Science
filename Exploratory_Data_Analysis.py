21f6f8c961414be57e4e704503243d3adbce9874
Akshaj Sinha <akshaj17@gmail.com>
2026-05-29 16:29:38 -0400

    starting on EDA phase

--- Exploratory_Data_Analysis.py
+++ Exploratory_Data_Analysis.py
@@ -1,6 +1,6 @@
 #Do teams that win have significantly more possession that teams that lose?
 import pandas as pd
-df= pd.read_csv("worldcup2022.csv")
+df = pd.read_csv('wc2022_cleaned.csv')
 
 def winner(column):
     if column['number of goals team1'] > column['number of goals team2']:
@@ -11,3 +11,5 @@ def winner(column):
         return 'draw'
 
 df['result'] = df.apply(winner, axis=1)
+
+print(df[['team1', 'team2', 'number of goals team1', 'number of goals team2', 'result']].head(10))
